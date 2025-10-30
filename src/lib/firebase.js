0c9ff5d6930bf544df7b9d8f50111b5fea75e2ed
Francisco Gatti <fragatdev@gmail.com>
2025-10-30 23:43:59 +0100

    test

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,6 +13,8 @@ const firebaseConfig = {
 	appId: import.meta.env.VITE_FIREBASE_APP_ID || 'YOUR_APP_ID'
 };
 
+console.log('Firebase Config:', firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
